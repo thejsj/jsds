52435946c88b2fa86ab4bfa1aedcebdc3cffe72c
Loring Dodge <loringdodge@yahoo.com>
2015-07-07 10:55:09 -0700

    Add exectimer and modify tests for bubbleSort

--- test/sorts/bubble-sort.js
+++ test/sorts/bubble-sort.js
@@ -1,14 +1,30 @@
 /*global describe: true, it:true */
 import should from 'should';
-import bubbleSort from '../../lib/sorts/bubble-sort';
+import { bubbleSort, t } from '../../lib/sorts/bubble-sort';
 
 describe('Bubble Sort', () => {
+
+  let arr;
+
+  beforeEach(() => {
+    arr = [3, 6, 7, 1, 4, 8, 9, 2, 5];
+  });
+
+  it('should throw an error when trying to add anything that\'s not an array', ()  => {
+    bubbleSort('2').should.throw();
+    bubbleSort(2).should.throw();
+    bubbleSort({ hello: 2 }).should.throw();
+    bubbleSort(undefined).should.throw();
+    bubbleSort(null).should.throw();
+  });
 
   it('should sort an array', ()  => {
     let arr = [3, 6, 7, 1, 4, 8, 9, 2, 5];
     bubbleSort(arr).should.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
+  it('should log execution time', ()  => {
+    console.log("Time:" + t.timers.bubbleSort.parse(t.timers.bubbleSort.duration()));
   });
 
 });
