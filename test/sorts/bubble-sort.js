4992a24b9d60e51b7c085db9810cede6f346cf29
Loring Dodge <loringdodge@yahoo.com>
2015-07-08 09:35:46 -0700

    Add time/space complexity comments

--- test/sorts/bubble-sort.js
+++ test/sorts/bubble-sort.js
@@ -19,7 +19,6 @@ describe('Bubble Sort', () => {
   });
 
   it('should sort an array', ()  => {
-    let arr = [3, 6, 7, 1, 4, 8, 9, 2, 5];
     bubbleSort(arr).should.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
