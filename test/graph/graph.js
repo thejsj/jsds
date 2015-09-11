dcfb7095c61c437013c806f1440aebe13c803f7a
thejsj <jorge.silva@thejsj.com>
2015-09-10 23:40:10 -0700

    Add graph functionality. Add tests

--- test/graph/graph.js
+++ test/graph/graph.js
@@ -4,10 +4,63 @@ import should from 'should';
 
 describe.only('Graph', () => {
 
-  describe('Insert', () => {
+  let graph;
+  beforeEach(() => {
+    graph = new Graph();
+  });
 
-    it('should insert a node into the graph', () => {
+  describe('addNode', () => {
+
+    it('should insert a single node into the graph', () => {
+      graph.addNode('hello', 1);
+      graph.getAllKeys().should.eql(['hello']);
     });
+
+    it('should insert a three nodes into the graph', () => {
+      graph.addNode('hello', 1);
+      graph.addNode('wow', 1, 'hello');
+      graph.addNode('great', 1, 'hello');
+      graph.getAllKeys().sort().should.eql(['great', 'hello', 'wow'].sort());
+    });
+
+    it('should insert a five nodes into the graph, connected to each other', () => {
+      graph.addNode('1', 1);
+      graph.addNode('2', 1, '1');
+      graph.addNode('3', 1, '2');
+      graph.addNode('4', 1, '3');
+      graph.addNode('5', 1, '4');
+      graph.addEdge('1', '5');
+      graph.getAllKeys().sort().should.eql(['1', '2', '3', '4', '5'].sort());
+    });
+
+  });
+
+  describe('containsNode', () => {
+
+  });
+
+  describe('removeNode', () => {
+
+  });
+
+  describe('Edges', () => {
+
+    describe('addEdge', () => {
+
+    });
+
+    describe('removeEdge', () => {
+
+    });
+
+    describe('setEdgeValue/getEdgeValue', () => {
+
+    });
+
+  });
+
+
+  describe('Iterators', () => {
 
   });
 
