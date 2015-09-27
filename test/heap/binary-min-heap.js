e53e4241a26795f4f41fae4833d740f5df27eb95
thejsj <jorge.silva@thejsj.com>
2015-09-27 01:20:50 -0700

    Adding initial API for BinaryMinHeap

---
+++ test/heap/binary-min-heap.js
@@ -0,0 +1,7 @@
+/*global describe: true, it:true, beforeEach:true */
+import should from 'should';
+import BinaryMinHeap from '../../lib/heap/binary-min-heap.js';
+
+describe('BinaryMinHeap', () => {
+
+});
---
+++ test/heap/index.js
@@ -0,0 +1,9 @@
+/*global describe:true */
+
+describe('Heap', () => {
+  describe('Binary Heap', () => {
+    require('./binary-min-heap.js');
+  });
+});
+
+
