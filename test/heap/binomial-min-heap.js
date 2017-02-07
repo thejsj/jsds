6b4ca9c89678c721f50af023f5e48a33a1c589c1
thejsj <jorge.silva@thejsj.com>
2017-02-07 08:55:19 -0800

    Add peek and pop operations

--- test/heap/binomial-min-heap.js
+++ test/heap/binomial-min-heap.js
@@ -11,24 +11,91 @@ describe('Binomail Min Heap', () => {
 
   describe('Insert', () => {
     it('should insert one key/value pair', () => {
+      expect(tree.validate()).to.equal(true)
       expect([...tree.entries()]).to.deep.equal([[20, 'hello']])
     })
 
     it('should insert two key/value pairs', () => {
       tree.insert(30, 'wow')
+      expect(tree.validate()).to.equal(true)
       expect([...tree.entries()]).to.deep.equal([[20, 'hello'], [30, 'wow']])
+    })
+
+    it('should insert 20 key/value pairs', () => {
+      let nums = [20]
+      for (let i = 0; i < 20; i += 1) {
+        let n = Math.random() * 100
+        nums.push(n)
+        tree.insert(n, 'wow')
+        expect(tree.validate()).to.equal(true)
+      }
+      expect([...tree.keys()].sort()).to.deep.equal(nums.sort())
     })
   })
 
   describe('Find Minimum', () => {
+    it('should find the min with one key/value pair', () => {
+      expect(tree.validate()).to.equal(true)
+      expect(tree.findMin()).to.deep.equal([20, 'hello'])
+    })
 
-  })
+    it('should find the min with three key/value pairs', () => {
+      expect(tree.validate()).to.equal(true)
+      tree.insert(10, 'wow')
+      expect(tree.validate()).to.equal(true)
+      tree.insert(30, 'wow')
+      expect(tree.peek()).to.deep.equal([10, 'wow'])
+    })
 
-  describe('Merge', () => {
-
+    it('should find the min with 20 key/value pairs', () => {
+      let min = [20]
+      for (let i = 0; i < 20; i += 1) {
+        let n = Math.floor(Math.random() * 1000)
+        min = Math.min(n, min)
+        expect(tree.validate()).to.equal(true)
+        tree.insert(n, 'hello')
+      }
+      expect(tree.peek()).to.deep.equal([min, 'hello'])
+    })
   })
 
   describe('Delete Minimum', () => {
+    it('should delete the min one key/value pair', () => {
+      expect(tree.validate()).to.equal(true)
+      expect(tree.peek()).to.deep.equal([20, 'hello'])
+      expect(tree.pop()).to.deep.equal([20, 'hello'])
+      expect([...tree.keys()].sort()).to.deep.equal([])
+    })
+
+    it('should delete the min three key/value pairs', () => {
+      expect(tree.validate()).to.equal(true)
+      tree.insert(10, 'wow')
+      expect(tree.validate()).to.equal(true)
+      tree.insert(30, 'wow')
+      expect(tree.peek()).to.deep.equal([10, 'wow'])
+      expect(tree.pop()).to.deep.equal([10, 'wow'])
+      expect([...tree.keys()].sort()).to.deep.equal([20, 30])
+      expect(tree.pop()).to.deep.equal([20, 'hello'])
+      expect([...tree.keys()].sort()).to.deep.equal([30])
+    })
+
+    it('should delete the min 20 key/value pairs', () => {
+      let tree = new BinomialMinHeap()
+      let nums = []
+      for (let i = 20; i < 20; i += Math.floor(Math.random())) {
+        nums.push(i)
+        expect(tree.validate()).to.equal(true)
+        tree.insert(i, 'hello')
+      }
+      for (let i = 0; i < nums.length; i += i) {
+        expect(tree.pop()).to.deep.equal([i, 'hello'])
+        nums.shift()
+        expect([...tree.keys()].sort()).to.deep.equal(nums)
+      }
+    })
+  })
+
+  describe('Merge', () => {
 
   })
 
